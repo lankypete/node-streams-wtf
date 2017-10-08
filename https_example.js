027766e4ba60662da439ae269632cbf04a601cc2
lankypete <lankypete7@gmail.com>
2017-10-08 19:15:33 +0000

    it does nothing!

---
+++ https_example.js
@@ -0,0 +1,44 @@
+const da = Array(55).join('-');
+function cd() {console.log(da)}
+function c(i, d){if(d)console.log(da+'\n'+i+'\n'+da); else console.log(i);}
+function cc(desc, arg, d){
+  if(d) cd()
+  console.log(desc);console.log('typeof: ', typeof arg);console.dir(arg);
+  if(d) cd()
+}
+
+
+var https = require('https');
+c('ididit')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
